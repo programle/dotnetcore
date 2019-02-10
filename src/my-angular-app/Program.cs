c5493750a9788effb0eb8d3dd442ade760801d85
Rahul Gramle <rahul.gramle@gmail.com>
2019-02-10 16:28:03 -0600

    refactored program.cs

--- src/my-angular-app/Program.cs
+++ src/my-angular-app/Program.cs
@@ -17,8 +17,7 @@ namespace my_angular_app
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args)
+                                                                                    .UseStartup<Startup>();
     }
 }
